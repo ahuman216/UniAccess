fceefd50ffa7c33e2d3d0ee50cd733b20151cbdd
Snehi Patel <snehippatel@gmail.com>
2026-03-29 14:05:21 -0400

    restart

--- read.js
+++
@@ -1,8 +0,0 @@
-const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-const recognition = new SpeechRecognition();
-recognition.continuous = true;
-recognition.onresult = e => {
-  document.getElementById('output').innerText += e.results[e.results.length-1][0].transcript + '
-';
-};
-document.getElementById('start').onclick = () => recognition.start();
